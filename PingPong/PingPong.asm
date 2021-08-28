b210a139ae22a2184688f05cfa12b6d8964f6466
Aaron Huggins <huggins.aaron@outlook.com>
2021-08-27 22:01:23 -0700

    Designed playfield, paddle, and ball

--- PingPong/PingPong.asm
+++ PingPong/PingPong.asm
@@ -93,6 +93,8 @@
     ;Variables
         gamestate: .res 1 ;stores current game state
         buttons: .res 1 ;used for checking buttons pressed
+        BGPointer: .res 2 ;used to select address when loading background
+        SpritePointer: .res 2 ;used to select address when loading sprites
         ;Ball
             ballx: .res 1 ;ball horizontal position
             bally: .res 1 ;ball vertical position
@@ -142,6 +144,18 @@
         jsr vblankwait
 ;Setup
     setupFunctions:
+        jsr SetupPalettes
+        jsr LoadPlayStateBG
+        jsr LoadSprites
+        jsr LoadAttribute
+        FinishSetup:
+            LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
+            STA PPUCTRL
+            LDA #%00011010   ; enable sprites(5), enable background(4), enable background on left side(2)
+            STA PPUMASK
+            LDA #$00        ;tell the ppu there is no background scrolling
+            STA PPUSCROLL
+            STA PPUSCROLL
 
 ;Wait for vblank interrupt
     Forever:
@@ -149,27 +163,138 @@
 
 ;Vblank 
     VBLANK: ;Runs every frame
+        jsr DisplaySprites
+
+        rti
+;Game Functions
+    DisplaySprites:
+        lda #$00
+        sta OAMADDR ;Set low byte (00) of sprite RAM address
+        lda #$02
+        sta OAMDMA ;Set high byte (02) of sprite RAM address and begin DMA transfer
+        rts
 
 ;Graphics
-    ;Palettes
-        background_palette:
-            .byte $22,$27,$17,$0F	;background palette 1 starting at $3F00
-            .byte $22,$14,$17,$0F	;background palette 2
-            .byte $22,$15,$21,$0F	;background palette 3
-            .byte $22,$27,$17,$0F	;background palette 4
-            
-        sprite_palette:
-            .byte $22,$27,$35,$0F	;sprite palette 1 starting at $3F10
-            .byte $22,$27,$17,$0F	;sprite palette 2
-            .byte $22,$1C,$15,$14	;sprite palette 3
-            .byte $22,$02,$38,$3C	;sprite palette 4
+    ;Setup Palettes
+        SetupPalettes: 
+            lda PPUSTATUS ;Read PPU status to reset high/low byte latch to high
+            lda #$3F
+            sta PPUADDR ; store high byte of $3F00 address
+            lda #$00
+            sta PPUADDR ; store low byte of $3F00 address
 
-    ;Sprites
+            ;loop through the palette data to store it all into the ppu
+            ldx #$00 ;start at 0
+            LoadBackgroundPaletteLoop:
+                lda background_palette, x ; load from address PaletteData+x 
+                sta PPUDATA ; ppu data register
+                inx ;increment x
+                cpx #$10 ;compare x to decimal 16
+                bne LoadBackgroundPaletteLoop
+
+            ldx #$00
+            LoadSpritePaletteLoop:
+                lda sprite_palette, X
+                sta PPUDATA
+                inx
+                cpx #$10
+                bne LoadSpritePaletteLoop
+            rts
+
+        ;Palettes
+            background_palette:
+                .byte $00,$0F,$10,$30	;background palette 1 starting at $3F00
+                .byte $00,$0F,$10,$30	;background palette 2
+                .byte $00,$0F,$10,$30	;background palette 3
+                .byte $00,$0F,$10,$30	;background palette 4
+                
+            sprite_palette:
+                .byte $00,$0F,$10,$30	;sprite palette 1 starting at $3F10
+                .byte $00,$0F,$10,$30	;sprite palette 2
+                .byte $00,$0F,$10,$30	;sprite palette 3
+                .byte $00,$0F,$10,$30	;sprite palette 4
+
+    ;Setup Backgrounds
+        LoadPlayStateBG:
+            lda PPUSTATUS ;read PPU status to reset the high/low latch
+            lda #$24
+            sta PPUADDR ;write the high byte of $2000 address
+            lda #$00
+            sta PPUADDR ;write the low byte of $2000 address
+
+            lda #<PlayStateBG
+            sta BGPointer
+            lda #>PlayStateBG
+            sta BGPointer+1
+            
+            ldx #$00 ;start out at 0
+            ldy #$00
+            LoadPlayStateBGLoop:
+                lda (BGPointer),y
+                sta PPUDATA
+                iny
+                cpx #$03
+                bne :+
+                cpy #$C0
+                beq DoneLoadingPlayStateBG    
+            :
+                cpy #$00
+                bne LoadPlayStateBGLoop
+                inx
+                inc BGPointer+1
+                jmp LoadPlayStateBGLoop
+            DoneLoadingPlayStateBG:
+                rts
+    ;Setup Sprites
+        LoadSprites: 
+            ldx #$00
+            LoadBallSprite:
+                lda BallSprite, x
+                sta $0200, x
+                inx
+                cpx #$04 
+                bne LoadBallSprite
+            ldx #$00
+            LoadPaddleOneSprite:
+                lda PaddleOneSprite, x
+                sta $0204, x
+                inx
+                cpx #$0C ; 3 sprites, 4 x 3 = 12 ($0C)
+                bne LoadPaddleOneSprite
+            ; Sprites are ready!
+            rts
+        BallSprite:
+            .byte $80,$00,$00,$40
+        PaddleOneSprite:
+            .byte $60,$01,$00,$10
+            .byte $68,$02,$00,$10
+            .byte $70,$03,$00,$10
+
+    ;Setup Attribute
+        LoadAttribute:
+            lda PPUSTATUS
+            lda #$23
+            sta PPUADDR ;write the high byte of $23C0 address
+            lda #$C0
+            sta PPUADDR ;write the low byte of $23C0 address
+
+            ldx #$00
+            LoadAttributeLoop:
+                lda attribute,x
+                sta PPUDATA
+                inx 
+                cpx #$40
+                bne LoadAttributeLoop
+
+            rts
 
     ;Nametables
         ;Background
-        
+            PlayStateBG:
+                .incbin "PingPongPlayState.nam"
         ;Attribute
+            attribute:
+                .incbin "attributes.nam"
 
 .segment "VECTORS"
     .word VBLANK
@@ -177,4 +302,4 @@
     .word 0
 
 .segment "CHARS"
- 
+    .incbin "PingPongChar.chr"
